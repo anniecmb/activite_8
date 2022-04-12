a80813977d3748a3b6ab706b1873ed39fe5b186d
anniecmb <annieclaude.mb@gmail.com>
2022-04-12 15:40:38 -0400

    Ajout message normal

---
+++ .vscode/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#button').addEventListener('click', logMessage);
+
+function logDebug(){
+    logMessage()
+}
+  
+function logMessage() {
+    console.log(m);
+    var m = 'Message'
